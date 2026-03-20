fcdc0036911b7fe46bd62ca6783975fcc3ecf5f3
Arsineh Boodaghian Asl <arsineh.boodaghian@gmail.com>
2026-03-20 13:48:58 +0100

    Add files via upload

--- ai_control_agents.py
+++ ai_control_agents.py
@@ -7,24 +7,37 @@ class AdaptiveSimulationController(BaseController):
     def __init__(self, queue_network, controller_df):
         super().__init__(queue_network, controller_df)
         self.capacity_by_vertex = capacity_by_vertex
-        self.adaptive_bottleneck_threshold = int(controller_df[controller_df["Controller_agent"] == "Bottleneck_manager"].iloc[0]["threshold"])
+        self.adaptive_controller_name = self.get_adaptive_controller_name()
+        self.adaptive_bottleneck_threshold = int(
+            controller_df[controller_df["Controller_agent"] == self.adaptive_controller_name].iloc[0]["threshold"]
+        )
 
-    # Here I override the controller rule so the bottleneck manager uses the adaptive threshold instead of the fixed one
+    # Here I identify the controller that monitors a network vertex instead of an external variable such as electricity price
+    def get_adaptive_controller_name(self):
+        adaptive_rows = self.controller_df[self.controller_df["monitored_item"] != "electricity_price"]
+
+        if adaptive_rows.empty:
+            raise ValueError("No adaptive bottleneck controller rule was found.")
+
+        return adaptive_rows.iloc[0]["Controller_agent"]
+
+    # Here I override the controller rule so the adaptive bottleneck controller uses the adaptive threshold instead of the fixed one
     def get_controller_rule(self, controller_name):
         monitored_item, controlled_vertex, threshold, action, slowdown_factor = super().get_controller_rule(controller_name)
-        if controller_name == "Bottleneck_manager":
+
+        if controller_name == self.adaptive_controller_name:
             threshold = self.adaptive_bottleneck_threshold
 
         return monitored_item, controlled_vertex, threshold, action, slowdown_factor
 
-    # Here I update the adaptive bottleneck threshold according to the current number of products in the buffer
+    # Here I update the adaptive bottleneck threshold according to the current number of products in the monitored vertex
     def update_adaptive_threshold(self):
-        monitored_item, _, base_threshold, _, _ = super().get_controller_rule("Bottleneck_manager")
-        buffer_products = self.observe_state("Bottleneck_manager")
-        buffer_capacity = int(self.capacity_by_vertex[monitored_item])
+        monitored_item, _, base_threshold, _, _ = super().get_controller_rule(self.adaptive_controller_name)
+        monitored_products = self.observe_state(self.adaptive_controller_name)
+        monitored_capacity = int(self.capacity_by_vertex[monitored_item])
         base_threshold = int(base_threshold)
 
-        if buffer_products > base_threshold:
+        if monitored_products > base_threshold:
             self.adaptive_bottleneck_threshold = max(1, self.adaptive_bottleneck_threshold - 1)
-        elif buffer_products < base_threshold:
-            self.adaptive_bottleneck_threshold = min(buffer_capacity, self.adaptive_bottleneck_threshold + 1)
+        elif monitored_products < base_threshold:
+            self.adaptive_bottleneck_threshold = min(monitored_capacity, self.adaptive_bottleneck_threshold + 1)
